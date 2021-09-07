b60f073fbb983e88abb761cdb0d387b77817e2f6
Öztirak Yunus <oeztirak-yunus1516@hotmail.com>
2021-09-07 18:48:28 +0200

    Data model updated | Position, Offer, TypeOfDiscount Added

---
+++ BillingSoftwareBackend/BillingSoftware.Core/Entities/Offer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BillingSoftware.Core.Entities
+{
+    public class Offer : EntityObject
+    {
+
+    }
+}
---
+++ BillingSoftwareBackend/BillingSoftware.Core/Entities/Position.cs
@@ -0,0 +1,44 @@
+﻿using BillingSoftware.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BillingSoftware.Core.Entities
+{
+    public class Position
+    {
+        public Product Product { get; set; }
+        public double Quantity { get; set; }
+        public double Discount { get; set; }
+        public TypeOfDiscount TypeOfDiscount { get; set; } = TypeOfDiscount.Prozent;
+        public double TotalPriceNet
+        {
+            get
+            {
+                if(TypeOfDiscount == TypeOfDiscount.Prozent)
+                {
+                    return Product.SellingPriceNet * (1 - (Discount / 100)) * Quantity;
+                }
+                else
+                {
+                    return (Product.SellingPriceNet - Discount) * Quantity;
+                }
+            }
+        }
+
+        public double TotalPriceGross
+        {
+            get
+            {
+                if (TypeOfDiscount == TypeOfDiscount.Prozent)
+                {
+                    return Product.SellingPriceGross * (1 - (Discount / 100)) * Quantity;
+                }
+                else
+                {
+                    return (Product.SellingPriceGross - Discount) * Quantity;
+                }
+            }
+        }
+    }
+}
