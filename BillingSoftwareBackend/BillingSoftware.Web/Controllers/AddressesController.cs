94e833817f10873d8a93577952bf4360298dffa7
oeztirakyunus <oeztirak-yunus1516@hotmail.com>
2021-09-12 13:56:24 +0200

    All Controllers added

---
+++ BillingSoftwareBackend/BillingSoftware.Web/Controllers/AddressesController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BillingSoftware.Core.Entities;
+using BillingSoftware.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillingSoftware.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AddressesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Address>>> GetAddresses()
+        {
+            return await _context.Addresses.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Address>> GetAddress(int id)
+        {
+            var address = await _context.Addresses.FindAsync(id);
+
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return address;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAddress(int id, Address address)
+        {
+            if (id != address.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(address).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AddressExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Address>> PostAddress(Address address)
+        {
+            _context.Addresses.Add(address);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAddress", new { id = address.Id }, address);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Address>> DeleteAddress(int id)
+        {
+            var address = await _context.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            _context.Addresses.Remove(address);
+            await _context.SaveChangesAsync();
+
+            return address;
+        }
+
+        private bool AddressExists(int id)
+        {
+            return _context.Addresses.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BillingSoftwareBackend/BillingSoftware.Web/Controllers/OrderConfirmationsController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BillingSoftware.Core.Entities;
+using BillingSoftware.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillingSoftware.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderConfirmationsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderConfirmationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderConfirmation>>> GetOrderConfirmations()
+        {
+            return await _context.OrderConfirmations.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderConfirmation>> GetOrderConfirmation(int id)
+        {
+            var orderConfirmation = await _context.OrderConfirmations.FindAsync(id);
+
+            if (orderConfirmation == null)
+            {
+                return NotFound();
+            }
+
+            return orderConfirmation;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrderConfirmation(int id, OrderConfirmation orderConfirmation)
+        {
+            if (id != orderConfirmation.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orderConfirmation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderConfirmationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<OrderConfirmation>> PostOrderConfirmation(OrderConfirmation orderConfirmation)
+        {
+            _context.OrderConfirmations.Add(orderConfirmation);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrderConfirmation", new { id = orderConfirmation.Id }, orderConfirmation);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<OrderConfirmation>> DeleteOrderConfirmation(int id)
+        {
+            var orderConfirmation = await _context.OrderConfirmations.FindAsync(id);
+            if (orderConfirmation == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrderConfirmations.Remove(orderConfirmation);
+            await _context.SaveChangesAsync();
+
+            return orderConfirmation;
+        }
+
+        private bool OrderConfirmationExists(int id)
+        {
+            return _context.OrderConfirmations.Any(e => e.Id == id);
+        }
+    }
+}
