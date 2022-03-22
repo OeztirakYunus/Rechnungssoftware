1f5bc8d3da17faaa0235358c5207860e317ae950
Öztirak Yunus <43437195+OeztirakYunus@users.noreply.github.com>
2022-03-22 23:00:44 +0100

    Update

--- BillingSoftwareBackend/BillingSoftware.Web/Program.cs
+++ BillingSoftwareBackend/BillingSoftware.Web/Program.cs
@@ -1,3 +1,4 @@
+using BillingSoftware.Persistence;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,8 +18,9 @@ namespace BillingSoftware.Web
         public async static Task Main(string[] args)
         {
             var server = CreateHostBuilder(args).Build();
-            try{
-                using var scope = server.Services.CreateScope();
+            using var scope = server.Services.CreateScope();
+            try
+            {
                 var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
                 await roleManager.CreateAsync(new IdentityRole("Admin"));
                 await roleManager.CreateAsync(new IdentityRole("User"));
@@ -25,7 +28,25 @@ namespace BillingSoftware.Web
             catch(Exception ex){
                 Console.WriteLine(ex.Message);
             }
-            
+
+            var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+            var deleteDatabase = Environment.GetEnvironmentVariable("DELETE_DATABASE");
+            if (string.IsNullOrEmpty(deleteDatabase))
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Environment.CurrentDirectory)
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                var configuration = builder.Build();
+                Debug.Write(configuration.ToString());
+                deleteDatabase = configuration["DeleteDatabase"];
+            }
+
+            if (deleteDatabase.ToLower().Equals("true"))
+            {
+                await context.Database.EnsureDeletedAsync();
+            }
+            await context.Database.EnsureCreatedAsync();
+
             server.Run();
         }
 
